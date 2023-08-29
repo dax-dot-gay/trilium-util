303e71da5029ee69f7b7d51e3445ce173009012a
Dax Harris <matteovh@gmail.com>
2023-08-29 14:33:58 -0400

    Basic page layout

--- tr-util/src/App.tsx
+++ tr-util/src/App.tsx
@@ -1,4 +1,13 @@
-import { MantineProvider } from "@mantine/core";
+import {
+    AppShell,
+    Button,
+    Group,
+    Header,
+    MantineProvider,
+    Title,
+} from "@mantine/core";
+import { MdLogin } from "react-icons/md";
+import "./app.scss";
 
 function App() {
     return (
@@ -9,7 +18,31 @@ function App() {
                 colorScheme: "dark",
             }}
         >
-            <></>
+            <AppShell
+                className="app"
+                header={
+                    <Header p="xs" height={60} className="app-header">
+                        <Group position="apart">
+                            <Group spacing="sm" className="app-title">
+                                <img
+                                    src="/icon.png"
+                                    alt="Application Logo"
+                                    className="app-logo"
+                                />
+                                <Title order={3}>Trilium Utilities</Title>
+                            </Group>
+                            <Button
+                                leftIcon={<MdLogin size={20} />}
+                                variant="subtle"
+                            >
+                                Log In
+                            </Button>
+                        </Group>
+                    </Header>
+                }
+            >
+                <></>
+            </AppShell>
         </MantineProvider>
     );
 }
